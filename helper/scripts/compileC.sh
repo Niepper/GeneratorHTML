ec0dbf546458f5057a58f5b2373347d8c010e597
Niepper <niepper@protonmail.com>
2024-04-04 18:36:31 +0200

    Remade file structure

--- helper/scripts/compileC.sh
+++ helper/scripts/compileC.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Specify the directory
-directory="../cpp/source"
+directory="../../code/cpp/source"
 
 # Check if the directory exists
 if [ ! -d "$directory" ]; then
@@ -14,13 +14,13 @@ if [ $# -eq 0 ]; then
   # Check if the file is a regular file
     if [ -f "$file" ]; then
       file_no_extension=$(basename "$file" .cpp)
-      g++ -shared -o ../cpp/lib/$file_no_extension.so -fPIC $file
-      x86_64-w64-mingw32-g++  -shared -o ../cpp/lib/$file_no_extension.dll -fPIC $file
+      g++ -shared -o ../../code/cpp/lib/$file_no_extension.so -fPIC $file
+      x86_64-w64-mingw32-g++  -shared -o ../../code/cpp/lib/$file_no_extension.dll -fPIC $file
     fi
   done
 else
   file=$1
   file_no_extension=$(basename "$file" .cpp)
-  g++ -shared -o ../cpp/lib/$file_no_extension.so -fPIC $file
-  x86_64-w64-mingw32-g++  -shared -o ../cpp/lib/$file_no_extension.dll -fPIC $file
+  g++ -shared -o ../../code/cpp/lib/$file_no_extension.so -fPIC $file
+  x86_64-w64-mingw32-g++  -shared -o ../../code/cpp/lib/$file_no_extension.dll -fPIC $file
 fi
