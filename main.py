aa9df6bb939fcf7ea5abff59f110877b8559f13b
Niepper <niepper@protonmail.com>
2024-04-04 16:31:09 +0200

    Finished example

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-
+from python.ctypesHandler import loadLib, libFullPath, LIB_PATH
+from pathlib import Path
 
 if __name__ == '__main__':
-    print("Yahoo")
+    libname = "example"
+    a = libFullPath(libname)
+    cFunctions = loadLib(a)
+    print(cFunctions.add(5,76))
+
