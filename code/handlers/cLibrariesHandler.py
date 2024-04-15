1e1bbea788d9ac9e0ab6694eec376fe608d5ff68
Niepper <niepper@protonmail.com>
2024-04-15 21:16:30 +0200

    Moved BMI calculator to cLibrariesHandler.py

--- code/handlers/cLibrariesHandler.py
+++ code/handlers/cLibrariesHandler.py
@@ -2,6 +2,8 @@ import ctypes
 import os
 from pathlib import Path
 
+from classes.person import Person
+
 LIB_PATH = "./code/cpp/lib/"
 LIB_EXTENSION = {"POSIX": ".so", "WIN": ".dll"}
 
@@ -22,3 +24,11 @@ def loadLib(filePath: str):
 def ToCArray(pyList: list, itemType=ctypes.c_int):
     return (itemType * len(pyList))(*list(map(lambda x: ctypes.c_int(int(x)), pyList)))
 
+
+def calcBMI(osoby: list[Person]):
+    calc = loadLib("example").kalkulator
+    for i in osoby:
+        calc.argtypes = [ctypes.c_float, ctypes.c_float]
+        calc.restype = ctypes.c_float
+        i.BMI = round(calc(ctypes.c_float(i.waga), ctypes.c_float(i.wzrost)), 2)
+    return osoby
--- code/main.py
+++ code/main.py
@@ -2,17 +2,12 @@ import ctypes
 from pathlib import Path
 
 from classes.person import Person
-from handlers.cLibrariesHandler import loadLib, libFullPath, ToCArray
+from handlers.cLibrariesHandler import loadLib, libFullPath, ToCArray, calcBMI
 from website.flaskBackend import startWebApp
 from handlers.csvHandler import readCSV
 
 if __name__ == '__main__':
     a = readCSV(Path("./code/website/exported/BMI CALC SMALL.csv").absolute())
 
-    b = loadLib("example").kalkulator
+    a = calcBMI(a)
 
-    for i in a:
-        b.argtypes = [ctypes.c_float, ctypes.c_float]
-        b.restype = ctypes.c_float
-        i.BMI = round(b(ctypes.c_float(i.waga), ctypes.c_float(i.wzrost)),2)
-        print(i.BMI)
