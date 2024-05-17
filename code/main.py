642fbd694d23fb5e0df7dd8f0fa18677651af7c2
Niepper <niepper@protonmail.com>
2024-05-17 09:48:19 +0200

    Removed unnecessary imports

--- code/main.py
+++ code/main.py
@@ -1,12 +1,3 @@
-import os
-from pathlib import Path
-import datetime
-
-from backend.handlers.cLibrariesHandler import calcBMI
-from backend.handlers.csvHandler import readCSV, sortPeople
-from backend.handlers.htmlHandler import generateHTML
-import re
-
 from frontend.website.websiteHandler import startWebApp
 
 if __name__ == '__main__':
