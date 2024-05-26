a0c53c85fb7dbd683b44cd9bfc2b3c91cbdab278
Niepper <niepper@protonmail.com>
2024-05-26 11:53:51 +0200

    sync

--- code/init.py
+++ code/init.py
@@ -8,13 +8,8 @@ import waitress
 
 
 def initialize():
-    # waitress.serve(website, host="127.0.0.1", port=5331)
-    currFilePath = Path("./frontend/website/TESTDATA/BMI CALC.csv")
-    people = readCSV(currFilePath.absolute())
-    people = calcBMI(people)
-    people = sortPeople(people)
-    people = getBMICattegory(people)
-    generatedPath = generateHTML(people, currFilePath)
+    waitress.serve(website, host="127.0.0.1", port=5331)
+
 
 
 if __name__ == "__main__":
